348b005cd4a346aa0fbdb9ffa44537501aed697e
Chris Vasey <chrismvasey@gmail.com>
2020-04-11 17:10:19 +0100

    Added function to get the pretty value of a card

--- app/Card.php
+++ app/Card.php
@@ -15,4 +15,45 @@ class Card extends Model
     {
         return $this->belongsTo('App\Player');
     }
+
+    public function prettyValue()
+    {
+        $suit = null;
+        $value = null;
+
+        switch ($this->suit) {
+            case 'S':
+                $suit = 'Spades';
+                break;
+            case 'H':
+                $suit = 'Hearts';
+                break;
+            case 'D':
+                $suit = 'Diamonds';
+                break;
+            case 'C':
+                $suit = 'Clubs';
+                break;
+        }
+
+        switch ($this->number) {
+            case 'J':
+                $value = 'Jack';
+                break;
+            case 'Q':
+                $value = 'Queen';
+                break;
+            case 'K':
+                $value = 'King';
+                break;
+            case 'A':
+                $value = 'Ace';
+                break;
+            default:
+                $value = $this->number;
+                break;
+        }
+
+        return $value.' of '.$suit;
+    }
 }
